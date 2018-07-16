f0d7c29de738c54e869f745494974522072d27ce
kuzin.al <kuzi4kin@gmail.com>
2018-07-16 17:11:03 +0300

    Initial commit

---
+++ src/main/java/com/example/S7SCADA/S7ScadaApplication.java
@@ -0,0 +1,12 @@
+package com.example.S7SCADA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S7ScadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S7ScadaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/S7SCADA/S7ScadaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.S7SCADA;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class S7ScadaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
